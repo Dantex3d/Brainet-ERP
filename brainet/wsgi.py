821f5b136a8fdf194c71b170e2887b9ebf459722
Dantex3d <merkoudaniel@gmail.com>
2026-06-07 23:37:20 +0300

    Update wsgi.py

--- brainet/wsgi.py
+++ brainet/wsgi.py
@@ -6,7 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
+from brainet.create_superuser import create_superuser
 
+create_superuser()
 import os
 
 from django.core.wsgi import get_wsgi_application
