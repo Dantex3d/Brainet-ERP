c268407809ef2761fbb18a31bdd20db40f6eb1a7
Dantex3d <your.merkoudaniel@gmail.com>
2026-06-15 20:45:11 +0300

    updates

--- brainet/settings.py
+++ brainet/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get(
     "django-insecure-development-key"
 )
 
-DEBUG = os.environ.get("DEBUG", "False") == "False"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
